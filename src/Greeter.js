d3c4db814276b558b3af0d3d4279e8399c2a9fdc
Ugo Arzur <ugo@payname.fr>
2016-07-25 20:25:42 +0200

    add react and react-dom

--- src/Greeter.js
+++ src/Greeter.js
@@ -1,7 +1,14 @@
-var config = require('./config.json');
+import React, {Component} from 'react'
+import config from './config.json';
 
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = config.greetText;
-  return greet;
-};
+class Greeter extends Component{
+  render() {
+    return (
+      <div>
+        {config.greetText}
+      </div>
+    );
+  }
+}
+
+export default Greeter
