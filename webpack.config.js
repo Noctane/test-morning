5afe9f482b48bd1018405b634b4f5e93de37df1b
Ugo Arzur <ugo@payname.fr>
2016-07-25 20:37:16 +0200

    add a node script to build eslint config and clean code webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 module.exports = {
   devtool: 'eval-source-map',
-  entry:  __dirname + "/src/main.js",
+  entry:  __dirname + '/src/main.js',
   output: {
-    path: __dirname + "/public",
-    filename: "bundle.js"
+    path: __dirname + '/public',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
         test: /\.json$/,
-        loader: "json"
+        loader: 'json'
       },
       { test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
       }
     ]
   },
   devServer: {
-    contentBase: "./public",
+    contentBase: './public',
     colors: true,
     historyApiFallback: true,
     inline: true
-  } 
-}
+  }
+};
