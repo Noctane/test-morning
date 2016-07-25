0dff66c748d42a3d7846fc785b2ae1177ac96754
Ugo Arzur <ugo@payname.fr>
2016-07-25 20:32:27 +0200

    add eslint support with airbnb styleguide

--- src/Greeter.js
+++ src/Greeter.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import config from './config.json';
 
-class Greeter extends Component{
+class Greeter extends Component {
   render() {
     return (
       <div>
@@ -11,4 +11,4 @@ class Greeter extends Component{
   }
 }
 
-export default Greeter
+export default Greeter;
