a2cf798167af088dbacebfe9a19fbf9abf69623f
Ugo Arzur <ugo@payname.fr>
2016-08-11 14:02:34 +0200

    refactor stateless component

--- src/greeter/Greeter.js
+++ src/greeter/Greeter.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import config from '../config.json';
+import React from 'react';
+import config from '../datas.json';
 import styles from './Greeter.css';
 
-class Greeter extends Component {
-  render() {
-    return (
-      <div className={styles.root}>
-        {config.greetText}
-      </div>
-    );
-  }
-}
+const Greeter = () =>
+  <div className={styles.root}>
+    {config.greetText}
+  </div>;
 
 export default Greeter;
