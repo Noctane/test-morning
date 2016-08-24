48f79067d5d21a8784015de69013861e958c0ec5
Ugo Arzur <ugo@payname.fr>
2016-08-24 23:28:37 +0200

    edit link app

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './app/App';
+import App from './app/app';
 require('./common.scss');
 
 render(<App />, document.getElementById('root'));
