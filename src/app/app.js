4ddab0ca9c1c899aba14480573e738bcf1255072
ugo <ugo.arzur@morning.com>
2017-04-05 11:42:30 +0200

    préparation pour arnaud

--- src/app/app.js
+++ src/app/app.js
@@ -1,23 +1,11 @@
 import React, { Component, PropTypes } from 'react';
-import Home from './home/home.js';
-//import { AppBar } from 'react-toolbox/lib/app_bar';
+// import { AppBar } from 'react-toolbox/lib/app_bar';
 require('./app.scss');
 
-class App extends Component {
-  static propTypes = {
-    isAuth: PropTypes.bool
-  };
-  static defaultProps = {
-    isAuth: false
-  };
+const App = () => (
+  <div className="app">
+    <h1>Hello Arnaud</h1>
+  </div>
+);
 
-  render() {
-    return (
-      <div className="app">
-        <h1>This is a title</h1>
-        <img src="https://unsplash.it/1024/768/?random" />
-      </div>
-    );
-  }
-}
 export default App;
--- src/app/appBar/appBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { AppBar } from 'react-toolbox/lib/app_bar';
-import theme from './appBar.scss';
-
-const appBar = (props) => (
-  <AppBar {...props} theme={theme}>This is the Appbar</AppBar>
-);
-
-export default appBar;
